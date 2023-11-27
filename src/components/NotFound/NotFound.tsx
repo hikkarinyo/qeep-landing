a279db9107c2e00783145c9b14a594f9c7b3ba5f
aleksandra loshkareva <aleksandra.loshkareva@qeep.pro>
2023-11-27 19:10:34 +0700

    fix NotFound

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import { Link } from 'react-router-dom'
 
 
 const cx = classNames.bind(require('./styles.scss'))
@@ -7,9 +8,10 @@ const cx = classNames.bind(require('./styles.scss'))
 const NotFound = () => {
     return (
         <div className={cx('not-found')}>
-            <div className={cx('container')}>
-            <h2 className={cx('not-found__title')}>404</h2>
-            <p className={cx('not-found__sub-title')}>Упс... страница не найдена</p>
+            <div className={cx('not-found__wrapper','container')}>
+                <h2 className={cx('not-found__title')}>404</h2>
+                <p className={cx('not-found__sub-title')}>Упс... страница не найдена</p>
+                <Link to='/' className={cx('not-found__link')}>Вернуться назад</Link>
             </div>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,8 +11,6 @@ import Testimonials from '../components/Testimonials/Testimonials'
 import Portfolio from '../components/Portfolio/Portfolio'
 import VideoSection from '../components/VideoSection/VideoSection'
 import { Loader } from '../common/Loader/Loader'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
 
 const Home = () => {
     const location = useLocation()
@@ -40,7 +38,6 @@ const Home = () => {
             {isLoading
                 ? <Loader/>
                 : <>
-                    <Header/>
                     <Banner id='banner'/>
                     <Benefit id='benefit'/>
                     <Possibilities id='possibilities'/>
@@ -51,7 +48,6 @@ const Home = () => {
                     <Calculator id='calculator'/>
                     <Testimonials id='testimonials'/>
                     <VideoSection/>
-                    <Footer/>
                     <ToastContainer autoClose={3000} />
                 </>
             }
