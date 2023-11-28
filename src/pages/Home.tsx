21f32af19c2ca2997feac04d6c24281c43cc985f
aleksandra loshkareva <aleksandra.loshkareva@qeep.pro>
2023-11-28 09:42:37 +0700

    fix NotFound x2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,8 @@ import Testimonials from '../components/Testimonials/Testimonials'
 import Portfolio from '../components/Portfolio/Portfolio'
 import VideoSection from '../components/VideoSection/VideoSection'
 import { Loader } from '../common/Loader/Loader'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 const Home = () => {
     const location = useLocation()
@@ -38,6 +40,7 @@ const Home = () => {
             {isLoading
                 ? <Loader/>
                 : <>
+                    <Header />
                     <Banner id='banner'/>
                     <Benefit id='benefit'/>
                     <Possibilities id='possibilities'/>
@@ -48,6 +51,7 @@ const Home = () => {
                     <Calculator id='calculator'/>
                     <Testimonials id='testimonials'/>
                     <VideoSection/>
+                    <Footer />
                     <ToastContainer autoClose={3000} />
                 </>
             }
